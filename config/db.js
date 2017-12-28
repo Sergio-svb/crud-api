d186ffbf53863c7bfb646c4a6a3df82d074cda0c
Sergio-svb <svb122@gmail.com>
2017-12-28 13:15:01 +0300

    test API repository initial

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: 'mongodb://serj:serj-pass_mdb@ds133127.mlab.com:33127/sergiomdb'
+};
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
+const db             = require('./config/db');
 const app            = express();
+
 let episodes = require('./routes/episodes');
+let users = require('./routes/users');
 const port = 8000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/episodes', episodes);
+app.use('/users', users);
 
 app.listen(port, () => {
     console.log('We are live on ' + port);
